b65e4558479a46c55fe385bc246ea2b35a7a48cc
emilien <emilien.charbonnier@cegetel.net>
2010-11-05 21:45:42 +0100

    first one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geopromo::Application.config.secret_token = 'd2db5ff91d221381ebe31996d46a37a71839307b5b0f5d32aecd9758a92c7822a7ed0702753ffebe6885c0d25f09cf5ba7e2df6965e7847e742409ae31e1ed88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Geopromo::Application.config.session_store :cookie_store, :key => '_geopromo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Geopromo::Application.config.session_store :active_record_store
