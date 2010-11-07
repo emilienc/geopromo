3c5ef39b38b9239b4db37eb6a2de699d440d64da
emilien <emilien.charbonnier@cegetel.net>
2010-11-07 10:45:00 +0100

    Ajout de devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ Geopromo::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  
+  config.action_mailer.default_url_options = { :host => 'geopromo.heroku.com' }
+  
 end
